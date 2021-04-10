9e7d3f2e469d233be40104552073c7543c4c0b98
Igor Stuk <independent.frd@gmail.com>
2021-04-10 22:00:21 +0300

    axios fix

--- src/components/searchApi.js
+++ src/components/searchApi.js
@@ -5,34 +5,98 @@ const apiKey = "ae0cace2b608fa732224591b2c086b58";
 // axios.defaults.headers.common["Authorization"] =
 //   "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZTBjYWNlMmI2MDhmYTczMjIyNDU5MWIyYzA4NmI1OCIsInN1YiI6IjYwNWY0MWUzNDZmMzU0MDAzYzYwMGFmMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aEPds4AK-Hx3jqxvZQgj_Fr8147zi0HudymQuYc7amc";
 
-const trending = () => {
-  return axios.get(
-    `https://api.themoviedb.org/3/trending/all/day?api_key=${apiKey}`
-  );
+const BASE_URL = "https://api.themoviedb.org/3";
+axios.defaults.baseURL = BASE_URL;
+
+axios.defaults.params = {
+  api_key: apiKey,
+  language: "en-US",
 };
 
-const searchMovie = (query) => {
-  return axios.get(
-    `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${query}&page=1&include_adult=false`
-  );
+// const trending = () => {
+//   return axios.get(
+//     `${BASE_URL}/trending/all/day?api_key=${apiKey}`
+//   );
+// };
+
+const trending = async () => {
+  try {
+    const config = { url: "/trending/all/day" };
+    const data = await axios(config);
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log("error", { error });
+    return [];
+  }
 };
 
-const movieDetail = (id) => {
-  return axios.get(
-    `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=en-US`
-  );
+// const searchMovie = (query) => {
+//   return axios.get(
+//     `${BASE_URL}/search/movie?api_key=${apiKey}&language=en-US&query=${query}&page=1&include_adult=false`
+//   );
+// };
+
+const searchMovie = async (query) => {
+  try {
+    const config = { url: "/search/movie", params: { query } };
+    const data = await axios(config);
+    return data;
+  } catch (error) {
+    console.log("error", { error });
+    return [];
+  }
 };
 
-const movieCredits = (id) => {
-  return axios.get(
-    `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${apiKey}&language=en-US`
-  );
+// const movieDetail = (id) => {
+//   return axios.get(
+//     `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=en-US`
+//   );
+// };
+
+const movieDetail = async (id) => {
+  try {
+    const config = { url: `/movie/${id}` };
+    const data = await axios(config);
+    return data;
+  } catch (error) {
+    console.log("error", { error });
+    return [];
+  }
 };
 
-const movieReviev = (id) => {
-  return axios.get(
-    `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${apiKey}&language=en-US`
-  );
+// const movieCredits = (id) => {
+//   return axios.get(
+//     `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${apiKey}&language=en-US`
+//   );
+// };
+
+const movieCredits = async (id) => {
+  try {
+    const config = { url: `/movie/${id}/credits` };
+    const data = await axios(config, id);
+    return data;
+  } catch (error) {
+    console.log("error", { error });
+    return [];
+  }
+};
+
+// const movieReviev = (id) => {
+//   return axios.get(
+//     `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${apiKey}&language=en-US`
+//   );
+// };
+
+const movieReviev = async (id) => {
+  try {
+    const config = { url: `/movie/${id}/reviews` };
+    const data = await axios(config);
+    return data;
+  } catch (error) {
+    console.log("error", { error });
+    return [];
+  }
 };
 
 const search = {
